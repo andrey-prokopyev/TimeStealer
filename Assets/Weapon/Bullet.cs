6516c2ab114040b503a0bcccb4b489a0c30dec50
Andrey Prokopyev <andrey.prokopyev@gmail.com>
2018-07-16 22:15:22 +0300

    removed bullet speed field

--- Assets/Weapon/Bullet.cs
+++ Assets/Weapon/Bullet.cs
@@ -6,20 +6,17 @@ namespace Weapon
 {
     public class Bullet : MonoBehaviour, IPoolable<float, WeaponCharger.Charge, Vector3, Vector3, IMemoryPool>
     {
-        //TODO : fix fireball speed
-        private float speed;
-
         private IMemoryPool pool;
 
         public void OnSpawned(float speed, WeaponCharger.Charge charge, Vector3 direction, Vector3 position, IMemoryPool pool)
         {
-            this.speed = speed;
             this.GetComponent<Damager>().Damage = charge.Current;
             this.pool = pool;
 
             this.transform.position = position;
             this.transform.rotation = Quaternion.LookRotation(direction);
 
+            // todo: refactor code below pls
             var fireball = this.transform.Find("Fireball");
             var projectilesParticles = fireball.Find("FireballProjectiles").GetComponent<ParticleSystem>().main;
             var bulletSpeed = speed * (1 - charge.Current / (2 * charge.Max));
@@ -49,11 +46,6 @@ namespace Weapon
                 Debug.LogFormat("{0} has left level boundary", this.gameObject.name);
                 this.pool.Despawn(this);
             }
-        }
-
-        public override string ToString()
-        {
-            return string.Format("Name: {0}, Speed: {1}", this.name, speed);
         }
 
         public class Factory : PlaceholderFactory<float, WeaponCharger.Charge, Vector3, Vector3, Bullet>
